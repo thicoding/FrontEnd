a776a85a3abcfedf6ca46a01165515476946347c
thiago-fernandes <thicodes.com>
2025-03-01 16:32:41 -0300

    Navegação imperativa com Router API

--- RXJS/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ RXJS/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProdutoService } from '../services/produto.service';
 import { Produto } from '../models/produto';
 
@@ -12,12 +12,19 @@ export class EditarProdutoComponent implements OnInit {
 
   produto: Produto;
 
-  constructor(private route: ActivatedRoute, private produtoService: ProdutoService) { }
+  constructor(
+     private router: Router,
+     private route: ActivatedRoute,
+     private produtoService: ProdutoService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.produto = this.produtoService.obterPorId(params['id']);
     })
+  }
+  salvar(){
+    this.router.navigate(['/produtos'])
+
   }
 
 }
